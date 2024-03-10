71b9caad99666490c10e5e2c8cc405ab1af227c7
Leanne Holmes <lholmes24@my.bcit.ca>
2024-03-10 03:54:17 -0700

    Removed some unused code

--- public/scripts/pet_age.js
+++ public/scripts/pet_age.js
@@ -1,34 +1,24 @@
-// var petAgeDiv = document.getElementById("pet-age");
-
-// const createdDate = moment("data-registered-date"); // need creation date here
-// console.log("createdDate: ", createdDate);
-
-// const currentDate = moment();
-// const petAge = currentDate.diff(createdDate, "days");
-
-// petAgeDiv.innerHTML = petAge + " days old";
-
-document.addEventListener('DOMContentLoaded', function() {
-    calculatePetAge();
+document.addEventListener("DOMContentLoaded", function () {
+  calculatePetAge();
 });
 
 function calculatePetAge() {
-    var petAgeDiv = document.getElementById("pet-age");
-    
-    // Fetch the registered date from the 'data-registered-date' attribute of the 'pet-age' div
-    const registeredDateString = petAgeDiv.getAttribute('data-registered-date');
+  var petAgeDiv = document.getElementById("pet-age");
 
-    // Check if the registeredDateString is valid
-    if (registeredDateString) {
-        const createdDate = moment(registeredDateString); // Use the fetched date here
-        console.log("createdDate: ", createdDate);
+  // Fetch the registered date from the 'data-registered-date' attribute of the 'pet-age' div
+  const registeredDateString = petAgeDiv.getAttribute("data-registered-date");
 
-        const currentDate = moment();
-        const petAge = currentDate.diff(createdDate, "days");
+  // Check if the registeredDateString is valid
+  if (registeredDateString) {
+    const createdDate = moment(registeredDateString); // Fetched date for pet creation
+    console.log("createdDate: ", createdDate);
 
-        petAgeDiv.innerHTML = petAge + " days old";
-    } else {
-        console.log("Registered date is missing or invalid.");
-        petAgeDiv.innerHTML = "Registered date is missing or invalid.";
-    }
+    const currentDate = moment();
+    const petAge = currentDate.diff(createdDate, "days");
+
+    petAgeDiv.innerHTML = petAge + " days old";
+  } else {
+    console.log("Registered date is missing or invalid.");
+    petAgeDiv.innerHTML = "Registered date is missing or invalid.";
+  }
 }
--- public/scripts/pet_status_display.js
+++ public/scripts/pet_status_display.js
@@ -3,36 +3,36 @@ const yellowRange = 50;
 
 //Updates the hunger,energy, and happiness bar using the pet's current stats.
 async function updateDisplay() {
-    //Get the stats from the server.
-    const response = await fetch(`/api/stats`);
-    var stats = await response.json();
+  //Get the stats from the server.
+  const response = await fetch(`/api/stats`);
+  var stats = await response.json();
 
-    //Hunger
-    updateColours("hunger", stats[0]);
-    document.getElementById("hunger").style.width = (stats[0]) + "%";
-    //Energy
-    updateColours("energy", stats[1]);
-    document.getElementById("energy").style.width = (stats[1]) + "%"
-    //Happiness
-    updateColours("happiness", stats[2]);
-    document.getElementById("happiness").style.width = (stats[2]) + "%"
+  //Hunger
+  updateColours("hunger", stats[0]);
+  document.getElementById("hunger").style.width = stats[0] + "%";
+  //Energy
+  updateColours("energy", stats[1]);
+  document.getElementById("energy").style.width = stats[1] + "%";
+  //Happiness
+  updateColours("happiness", stats[2]);
+  document.getElementById("happiness").style.width = stats[2] + "%";
 }
 
 //Updates the colour of the specified bar.
 function updateColours(id, newVal) {
-    var doc = document.getElementById(id);
-    var oldVal = Number(doc.style.width.substring(0,doc.style.width.length-1));
-    if(doc.classList.contains(petCondition(oldVal))) {
-        doc.classList.remove(petCondition(oldVal))
-    }
-    doc.classList.add(petCondition(newVal));
+  var doc = document.getElementById(id);
+  var oldVal = Number(doc.style.width.substring(0, doc.style.width.length - 1));
+  if (doc.classList.contains(petCondition(oldVal))) {
+    doc.classList.remove(petCondition(oldVal));
+  }
+  doc.classList.add(petCondition(newVal));
 }
 
 //Determine what "condition" a given value is in.
 function petCondition(value) {
-    if(value <= redRange) return "bad";
-    else if(value <= yellowRange) return "moderate";
-    else return "good"
+  if (value <= redRange) return "bad";
+  else if (value <= yellowRange) return "moderate";
+  else return "good";
 }
 
 //Updates the display upon loading the page.
