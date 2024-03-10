3cfc1be153c17d7d3124cad7818221e99bcf5878
Yerim Moon <myr9901@gmail.com>
2024-03-09 17:23:56 -0800

    display pet's name

--- index.js
+++ index.js
@@ -75,7 +75,8 @@ app.get("/main", async (req, res) => {
         res.redirect("/");
         return;
     }
-    res.render("main");
+    const pet = await petCollection.findOne({ user_id: req.session.userId });
+    res.render("main", { pet: pet });
 });
 
 app.get("/register_pet_type", (req, res) => {
@@ -162,6 +163,7 @@ app.post("/loginSubmit", async (req, res) => {
     .toArray();
 
   const uid = result[0]._id;
+
   await userCollection.updateOne(
     { _id: new ObjectId(uid) },
     { $set: { last_time_logged_in: new Date() } }
